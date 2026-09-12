62d071c735c2372b630ad8670501e85b72add051
Antonio Carlos <xdatabycarlos@gmail.com>
2026-09-12 15:29:23 -0300

    feat: Implementa Componente TotalCost.jsx

---
+++ src/TotalCost.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./TotalCost.css";
+
+const TotalCost = ({ totalCost, ItemsDisplay }) => {
+  const amount_total =
+    totalCost.venue +
+    totalCost.decor +
+    totalCost.sound +
+    totalCost.catering +
+    totalCost.coupleExperience +
+    totalCost.memories;
+
+  return (
+    <>
+      <div className="pricing">
+        <div className="display-box">
+          <header>
+            <h1>Custo Total por evento</h1>
+          </header>
+          <div className="total_amount">
+            <h2>R$ {amount_total}</h2>
+          </div>
+          <div className="render">
+            <ItemsDisplay/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TotalCost;
