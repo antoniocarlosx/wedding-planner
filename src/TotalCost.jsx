de2852ab874201c4b5880c9f032124719b057bef
Antonio Carlos <xdatabycarlos@gmail.com>
2026-09-12 16:01:16 -0300

    feat: Consolida Estilização de cores

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -15,10 +15,10 @@ const TotalCost = ({ totalCost, ItemsDisplay }) => {
       <div className="pricing">
         <div className="display-box">
           <header>
-            <h1>Custo Total por evento</h1>
+            <h1>Custo Total por Evento:</h1>
           </header>
           <div className="total_amount">
-            <h2>R$ {amount_total}</h2>
+            <h2>R$ {amount_total},00</h2>
           </div>
           <div className="render">
             <ItemsDisplay/>
--- src/WeddingPlanner.jsx
+++ src/WeddingPlanner.jsx
@@ -1,63 +1,92 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import "./WeddingPlanner.css"
+import "./WeddingPlanner.css";
 import TotalCost from "./TotalCost";
 
-
 const WeddingPlanner = () => {
-
   const [showDetails, setShowDetails] = useState(false);
 
-  return (
+  const totalCost = {
+    venue: 0,
+    decor: 0,
+    sound: 0,
+    catering: 0,
+    coupleExperience: 0,
+    memories: 0,
+  };
 
-    
+  const ItemsDisplay = () => <p>Nenhum Item selecionado</p>;
+
+  return (
     <>
       <header>
         <nav className="navbar_planner">
-            <div className="nav-logo">WeddingPlanner</div>
-            <div className="nav-left">
-                <div className="nav-links">
-                    <a href="#venue" className="nav-link">Local</a>
-                    <a href="#decor" className="nav-link">Decoração</a>
-                    <a href="#sound" className="nav-link">Som & Cerimônia</a>
-                    <a href="#catering" className="nav-link">Gastronomia & Recepção</a>
-                    <a href="#coupleExperience" className="nav-link">Experiência dos Noivos</a>
-                    <a href="#memories" className="nav-link">Memórias</a>
-
-                </div>
+          <div className="nav-logo">WeddingPlanner</div>
+          <div className="nav-left">
+            <div className="nav-links">
+              <a href="#venue" className="nav-link">
+                Local
+              </a>
+              <a href="#decor" className="nav-link">
+                Decoração
+              </a>
+              <a href="#sound" className="nav-link">
+                Som & Cerimônia
+              </a>
+              <a href="#catering" className="nav-link">
+                Gastronomia & Recepção
+              </a>
+              <a href="#coupleExperience" className="nav-link">
+                Experiência dos Noivos
+              </a>
+              <a href="#memories" className="nav-link">
+                Memórias
+              </a>
             </div>
+          </div>
 
-            <div className="nav-right">
-                <button className="details-btn" onClick={() => setShowDetails(!showDetails)}>Ver Detalhes</button>
-            </div>
+          <div className="nav-right">
+            <button
+              className="details-btn"
+              onClick={() => setShowDetails(!showDetails)}
+            >
+              Ver Detalhes
+            </button>
+          </div>
         </nav>
       </header>
+
+      {showDetails && (
+        <TotalCost totalCost={totalCost} ItemsDisplay={ItemsDisplay}/>
+      )}
+
+
 
       <section id="numberOfGuests">
         <h2>Para Quantos Convidados?</h2>
       </section>
 
-       <section id="venue">
+      <section id="venue">
         <h2>🌸 Local & Cerimônia</h2>
-         <p>Em qual local?</p>
+        <p>Em qual local?</p>
       </section>
 
       <section id="decor">
-        <h2>🌿 Decoração & Ambientação	</h2>
+        <h2>🌿 Decoração & Ambientação </h2>
         <p>Com que decoração?</p>
       </section>
 
-       <section id="sound">
+      <section id="sound">
         <h2>🎻 Som & Cerimônia</h2>
         <p>Como seria a sonorização?</p>
       </section>
 
-       <section id="catering">
+      <section id="catering">
         <h2>🥂 Gastronomia & Recepção</h2>
         <p>O que teria para comer?</p>
       </section>
 
-       <section id="coupleExperience">
+      <section id="coupleExperience">
         <h2>💐 Experiência dos Noivos</h2>
         <p>Quais experiências você quer ter com seu noivo?</p>
       </section>
@@ -65,11 +94,7 @@ const WeddingPlanner = () => {
       <section id="memories">
         <h2>📸 Memórias</h2>
         <p>Como quer guardar essas memórias?</p>
-
       </section>
-
-
-      
     </>
   );
 };
