72300a8a63d33ae5b5722de81d6271a000e9de68
Antonio Carlos <xdatabycarlos@gmail.com>
2026-09-14 16:29:44 -0300

    feat: registra o slice de catering na store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import decorReducer from "./decorSlice";
 import soundReducer from "./soundSlice";
+import cateringReducer from "./cateringSlice"
 
 export const store = configureStore({
   reducer: {
     decor: decorReducer,
     sound: soundReducer,
+    catering: cateringReducer
   },
 });
